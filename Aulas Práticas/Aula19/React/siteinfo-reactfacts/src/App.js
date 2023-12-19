c3cd7f41d3eb628546926bf99ac03c5d237e3e05
serviocardus <serviocardus@outlook.com>
2023-12-18 21:02:15 -0300

    Atualização_18-12-2023_21h01m

--- Aulas Práticas/Aula19/React/siteinfo-reactfacts/src/App.js
+++ Aulas Práticas/Aula19/React/siteinfo-reactfacts/src/App.js
@@ -1,12 +1,12 @@
 import './styles.css';
-import Header from './components/Header';
+import Navbar from './components/Navbar';
 import Main from './components/Main';
 import Footer from './components/Footer';
 
 export default function App() {
   return (
     <div>
-    <Header />
+    <Navbar />
     <Main />
     <Footer />
   </div>
---
+++ Aulas Práticas/Aula19/React/siteinfo-reactfacts/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <header>
+      <nav>
+      <img src="./images/logo-react.png" alt="logo react" width="40px" />
+        <h3 className ="nav-logo_text">ReactFacts</h3>
+        <h4 className="nav-title">React Course - Project 1</h4>
+      </nav>
+    </header>
+  );
+}
