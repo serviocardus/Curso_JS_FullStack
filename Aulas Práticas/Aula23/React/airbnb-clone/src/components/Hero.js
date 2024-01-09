e3cb302b4da5676c5449dd932c3ffadfe8c15238
serviocardus <serviocardus@outlook.com>
2024-01-08 21:16:17 -0300

    Atualização_08-01-2024_21h16m

---
+++ Aulas Práticas/Aula23/React/airbnb-clone/src/components/Hero.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <section className='hero'>
+        <img src="./images/photo-grid.png" alt="imagem grid" className='hero-grid'/>
+        <div className='hero-texto'>
+            <h1>Online Experiences</h1>
+            <p>
+                Join unique interactive activities led by
+                <br /> one-of-a-kind hosts-all without leving
+                <br /> home.
+            </p>
+        </div>
+    </section>
+  )
+}
---
+++ Aulas Práticas/Aula23/citacoesData.js
@@ -0,0 +1,24 @@
+const arr = [
+{
+citacao: "Creio no riso e nas lágrimas como antídotos contra o ódio e o terror.",
+autor: "Charles Chaplin"
+},
+{
+citacao: "Seja como for o que penses, creio que é melhor dizê-lo com boas palavras.",
+autor: "William Shakespeare" 
+},
+{
+citacao: "Creio que tenho prova suficiente de que falo a verdade: a pobreza.",
+autor: "Sócrates" 
+},
+{
+citacao: "Creio que quase sempre é preciso um golpe de loucura para se construir um destino.",
+autor: "Marguerite Yourcenar" 
+},
+{
+citacao: "Eu creio haver corações que poderiam cortar diamantes.",
+autor: "Emanuel Wertheimer"
+},
+]
+
+export default arr
